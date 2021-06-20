b9b3c4bc445b1dd0d33f3d0c8c25eade30c0b2e3
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-20 18:53:58 -0400

    just an example index page

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
 
-
-global $sitedir;
+//you can edit this file load the way you see fit, it's just an example, but index files always contain autoloading and/or requires
 
 require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'load_indexes.php';
 
+//edit below
+
+global $sitedir;
 $ldir = $sitedir . 'library' . DIRECTORY_SEPARATOR;
 
 nathanwooten\LoadIndexes\load_indexes( $ldir );
