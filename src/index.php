7f674bb73639c3b4aa5e9679985ce0946215ba0e
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-20 18:51:31 -0400

    fixed space, now underscore

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 
 global $sitedir;
 
-require once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'load_indexes.php';
+require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'load_indexes.php';
 
 $ldir = $sitedir . 'library' . DIRECTORY_SEPARATOR;
 
