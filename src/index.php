4194b7dce42a779d3a0310d223d816b93dd9021a
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-20 15:11:01 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
 <?php
 
+
+global $sitedir;
+
 require once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'load_indexes.php';
+
+$ldir = $sitedir . 'library' . DIRECTORY_SEPARATOR;
+
+load_indexes( $ldir );
